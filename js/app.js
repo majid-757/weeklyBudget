eddcaf69c691a8205051ca8fc3df8746b89d31c3
unknown <majid.asadollahi95@gmail.com>
2023-04-05 19:49:20 +0330

    Add-expense-to-html

--- js/app.js
+++ js/app.js
@@ -7,7 +7,53 @@ class Budget {
 }
 
 
+// everything related to the html
+class HTML {
 
+    // insert user budget to the html
+    insertBudget(amount){
+        budgetTotal.innerHTML = amount;
+        budgetLeft.innerHTML = amount;
+    }
+
+
+    printMessage(message, className){
+        const div = document.createElement('div');
+        div.classList.add('alert', 'text-center', className);
+        div.appendChild(document.createTextNode(message));
+
+
+        const primary = document.querySelector('.primary');
+        primary.insertBefore(div, addExpenseForm);
+
+
+        setTimeout(() => {
+            document.querySelector('.alert').remove();
+        }, 2000);
+
+        addExpenseForm.reset()
+
+    }
+
+
+    // display expenses to the list
+    insertExpense(name, amount) {
+        const expenses = document.querySelector('#expenses ul');
+
+        let li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+
+        li.innerHTML = `
+        ${name}
+
+        <span class="badge badge-primary badge-pill">${amount}</span>
+        `
+        expenses.appendChild(li);
+
+
+    }
+
+}        
 
 
 
@@ -15,7 +61,13 @@ class Budget {
 // variable
 let userBudget;
 let budget;
+let budgetTotal = document.querySelector('span#total');
+let budgetLeft = document.querySelector('span#left');
 
+
+const addExpenseForm = document.querySelector('#add-expense');
+
+const html = new HTML();
 
 
 
@@ -29,7 +81,6 @@ function eventlisteners() {
     // take a alert for user budget
     document.addEventListener('DOMContentLoaded', function() {
         userBudget = prompt('لطفا بودجه هفتگی خود را وارد کنید')
-        console.log(userBudget)
 
         // validate
         if (userBudget === null || userBudget === '' || userBudget === '0') {
@@ -37,11 +88,29 @@ function eventlisteners() {
         } else {
             // instanciate Budget class
             budget = new Budget(userBudget)
-            console.log(budget)
+            html.insertBudget(budget.budget)
         }
 
     })
 
+
+    // get valuse form the form when submited
+    addExpenseForm.addEventListener('submit', function(e) {
+        e.preventDefault()
+
+        // accsess to the value of input 
+        const expense = document.querySelector('#expense').value
+        const amount = document.querySelector('#amount').value
+
+        if (expense === '' || amount === '') {
+            html.printMessage('همه موارد الزامی است', 'alert-danger');
+        } else {
+            html.insertExpense(expense, amount)
+
+        }
+
+
+    })
 }
 
 
